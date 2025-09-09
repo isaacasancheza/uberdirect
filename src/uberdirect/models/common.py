1add995cdba1819ddc612c4d9012b8ceb8ff29ac
Isaac Sánchez <isaac@elrincondelsanchez.com>
2025-09-08 18:53:57 -0600

    :construction: add district to structured address

--- src/uberdirect/models/common.py
+++ src/uberdirect/models/common.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 
 class StructuredAddress(BaseModel):
     street_address: tuple[str] | tuple[str, str]
+    district: str
     city: str
     state: str
     zip_code: str
